07bc57dd3da16c92e9d561c1bbb5837e8343168c
istekhar khan <istekharkhan46@gmail.com>
2023-08-11 22:48:57 +0530

    learn typeScript

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict";
+// function sum(a: number, b: number): number {
+//     return a + b;
+// }
+// const add = (a: number, b: number): number => {
+//     return  a + b ;
+// }
+// let num:number = NaN;
+// // num = 20 + 'istekhar'
+// //  num = 30 
+//  console.log(num)
+// sum(5, 10);
+// let Name:string = 'istekhar lets lern TypeScript'
+// let last:string = 'khan'
+// let Length:number = Name.length; 
+// let myFullName:string;
+// let Upper:string = Name.toUpperCase();
+// let Lower:string = Name.toLowerCase();
+// let short:string = Name.substring(0,5)
+// console.log(short)
+// myFullName = Name+last
+// console.log(Upper)
+// console.log(Lower)
+// Day- 2 =>>>>>>>>>>>>>>>>>>>>>>
+const even = (a) => {
+    if (a % 2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+const iseven = (a) => {
+    return a % 4 === 0 || a % 8 === 0;
+};
+// const big:bigint = 4937389739875834834n
+console.log(iseven(5));
